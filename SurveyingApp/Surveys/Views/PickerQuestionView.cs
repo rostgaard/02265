f50bed84475151cba9acb3db5d393c2abdfd984f
sirVir <piotrek.milczarek@gmail.com>
2015-04-22 15:48:29 +0200

    code reorganisation and initial set of Views for the Survey

---
+++ SurveyingApp/Surveys/Views/PickerQuestionView.cs
@@ -0,0 +1,51 @@
+﻿using Xamarin.Forms;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+
+namespace Surveys
+ {
+	public class PickerQuestionView : QuestionView
+	{
+		Label QuestionLabel { get; set;}
+
+
+		Picker AnswerPicker { get; set;}
+
+
+
+		public PickerQuestionView ( string questionText, List<string> options) : this ()
+		{
+			QuestionLabel.Text = questionText;
+			foreach (string answer in options) {
+				AnswerPicker.Items.Add (answer);
+			}
+		}
+
+		public PickerQuestionView () : base()
+		{
+			QuestionLabel = new Label {
+				Text = "Pick the item you like!"
+			};
+
+			AnswerPicker = new Picker ();
+
+			AnswerPicker.Items.Add ("First option");
+			AnswerPicker.Items.Add ("Second option");
+			AnswerPicker.Items.Add ("Third option");
+			AnswerPicker.Items.Add ("Fourth option");
+
+			AnswerPicker.SelectedIndexChanged += (sender, e) => {
+				this.IsAnswered = true;
+			};
+
+			this.Children.Add (QuestionLabel);
+
+			this.Children.Add (AnswerPicker);
+
+		}
+
+
+
+	}
+}
---
+++ SurveyingApp/Surveys/Views/QuestionView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Surveys
+{
+	public abstract class QuestionView : StackLayout
+{
+	
+		public bool IsMandatory { get; set;}
+
+		bool isAnswered = false;
+		public bool IsAnswered {
+			get {
+				return isAnswered;
+			}
+			set {
+				if (isAnswered != value) {
+					isAnswered = value;
+					OnPropertyChanged("IsAnswered");
+					}
+
+			}
+		}
+
+		public QuestionView()
+		{
+			this.VerticalOptions = LayoutOptions.FillAndExpand;
+		}
+}
+
+}
