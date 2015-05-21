485f44d579257669213f00b1d6a6e9cc7f992f16
Anna Maria Walach <jahi.wh+git@gmail.com>
2015-05-21 17:22:25 +0200

    !important Scheduller workflow

--- SurveyingApp/Surveys/Model/Survey Instance/SurveyAnswer.cs
+++ SurveyingApp/Surveys/Model/Survey Instance/SurveyAnswer.cs
@@ -17,3 +17,20 @@ namespace Surveys
 	}
 }
 
+// start of app
+// check if any new schemas (schema that has no instance generated yet)
+// if new schema, generate new survey instance
+// foreach survey instance in unfilled instances folder do
+//		set Active flags (ScheduleController.setActive(surveyInstance))
+//		if surveyInstance.survey.IsActive then add survey Instance to display active list
+
+// app is displaying active instance
+// customer chooses one of them
+// he fills it in and saves
+// save actions:
+// serialize survey instance (with all the answer) to "done surveys" folder
+// call ScheduleController.reschedule(surveyInstance) <-- generated new dates (and may set active flags)
+// remove old surveyInstance_serialized from "unfilled instances" folder
+// add new surveyInstance_rescheduled to "unfilled instances" folder
+
+
