8a59acbb676fb2301b967591053921f4164905b9
Anna Maria Walach <jahi.wh+git@gmail.com>
2015-05-25 13:07:06 +0200

    Startup controller implementation.

--- SurveyingApp/Surveys/Controllers/StartupController.cs
+++ SurveyingApp/Surveys/Controllers/StartupController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Surveys
 {
@@ -6,6 +7,39 @@ namespace Surveys
 	{
 		public StartupController ()
 		{
+		}
+
+		public static List<Survey> chechForNewSchemas(List<Survey> schemas, List<SurveyAnswer> instances) 
+		{
+			List<SurveyAnswer> duplicate = new List<SurveyAnswer>(instances);
+			List<Survey> news = new List<Survey>();
+
+			foreach (Survey schema in schemas) {
+				Boolean founded = false;
+				foreach (SurveyAnswer instance in duplicate) {
+					if (schema.SurveyId.Equals (instance.Survey.SurveyId)) {
+						duplicate.Remove (instance);
+						founded = true;
+						break;
+					}
+				}
+				if (!founded) {
+					news.Add (schema);
+				}
+			}
+
+			return news;
+		}
+
+		public static List<SurveyAnswer> generateInstances(List<Survey> schemas, UserData userData) 
+		{
+			List<SurveyAnswer> instances = new List<SurveyAnswer> ();
+			foreach (Survey schema in schemas) 
+			{
+				SurveyAnswer instance = new SurveyAnswer (schema, userData);
+				instances.Add (instance);
+			}
+			return instances;
 		}
 	}
 }
