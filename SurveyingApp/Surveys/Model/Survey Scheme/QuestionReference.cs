87da117bb80ef2207ce4858f4f583679b0114153
sirVir <piotrek.milczarek@gmail.com>
2015-06-02 16:32:56 +0200

    Fixed prerequisites

--- SurveyingApp/Surveys/Model/Survey Scheme/QuestionReference.cs
+++ SurveyingApp/Surveys/Model/Survey Scheme/QuestionReference.cs
@@ -5,6 +5,9 @@ namespace Surveys
 {
 	public class QuestionReference
 	{
+
+		public Guid QuestionId { get; set; }
+
 		public Question Question { get; set; }
 
 		public List<Prerequisite> Prerequisites { get; set; }
@@ -12,15 +15,27 @@ namespace Surveys
 		public QuestionReference ()
 		{
 			Prerequisites = new List<Prerequisite> ();
+			QuestionId = Guid.NewGuid ();
 		}
 
-		public QuestionReference (Question q)
+		public QuestionReference (Question q) : this ()
 		{
 			Prerequisites = new List<Prerequisite> ();
 			Question = q;
 		}
 
+		public override int GetHashCode()
+		{
+			return QuestionId.GetHashCode ();
+		}
 
+		public override bool Equals(object o)
+		{
+			if (o is QuestionReference) {
+				return this.QuestionId.Equals (((QuestionReference)o).QuestionId);
+			}
+			return false;
+		}
 	}
 }
 
